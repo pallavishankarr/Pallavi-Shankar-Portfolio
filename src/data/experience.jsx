2e9224999dad6154b202e24e6e017b94269e3220
pallavishankarr <pshankar6996@gmail.com>
2025-08-29 14:39:00 -0700

    Added basic data experience and projects

---
+++ src/data/experience.jsx
@@ -0,0 +1,34 @@
+const experiences = [
+    {
+      id: 1,
+      title: "Research Assistant at Northeastern Brain Game Center",
+      description: "TBD",
+      timeline: "Incoming",
+    },
+    {
+      id: 2,
+      title: "Software Director at Northeastern iGEM",
+      description: "Finish description later",
+      timeline: "July 2025 - Present",
+    },
+    {
+      id: 3,
+      title: "Data Analyst Extern at Beats by Dr. Dre",
+      description: "I am working on a project to analyze the data of the Beats by Dr. Dre brand. I am using the Pandas library to analyze the data and the Matplotlib library to visualize the data.",
+      timeline: "July 2025 - Present",
+    },
+    {
+        id: 4,
+        title: "Teaching Assistant for CS2510 at Northeastern University",
+        description: "I am currently a teaching assistant for the Fundamentals of Computer Science 2 course at Northeastern. My responsibilities include helping students during office hours, during lab sessions, and grading assignments + exams. I will be helping TA during the course switchove from Java to Python.",
+        timeline: "May 2025 - Present",
+    },
+    {
+        id: 5,
+        title: "Software Engineering Intern at Secured Health",
+        description: "I helped develop platform. Add more description later.",
+        timeline: "May 2025 - August 2025",
+    }
+  ];
+  
+  export default experiences;
---
+++ src/data/projects.jsx
@@ -0,0 +1,11 @@
+const projects = [
+    {
+      id: 1,
+      title: "Co-Connect",
+      description: "add description later.",
+      technologies: ["Python", "Streamlit", "SQL", "Flask"],
+      link: "https://github.com/NiamBashambu/Co_Connect",
+    },
+  ];
+  
+  export default tprojects;
